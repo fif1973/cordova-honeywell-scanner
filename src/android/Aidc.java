4315df551bb9596734c3d2c74309d05d1ea36956
Philippe MAES <pmaes@nextoo.fr>
2021-08-04 14:28:28 +0200

    Enable UPC-A

--- src/android/Aidc.java
+++ src/android/Aidc.java
@@ -34,63 +34,73 @@ public class Aidc extends CordovaPlugin implements BarcodeListener {
 
 	private BarcodeReader barcodeReader;
 	private AidcManager manager;
-  private String mConnectedScanner = null;
+	private String mConnectedScanner = null;
 
 	private CallbackContext currentCallbackContext = null;
 
-  @Override
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
 
-    // We get the aidc manager at the initialization of the plugin
-    AidcManager.create(this.cordova.getActivity(), new CreatedCallback() {
-      @Override
-      public void onCreated(AidcManager aidcManager) {
-        manager = aidcManager;
+		// We get the aidc manager at the initialization of the plugin
+		AidcManager.create(this.cordova.getActivity(), new CreatedCallback() {
+			@Override
+			public void onCreated(AidcManager aidcManager) {
+				manager = aidcManager;
 
-        if (manager != null) Log.d(TAG, "manager aidc init success");
-        else Log.d(TAG, "manager aidc init failed");
-      }
-    });
-  }
+				if (manager != null) Log.d(TAG, "manager aidc init success");
+				else Log.d(TAG, "manager aidc init failed");
+			}
+		});
+	}
 
 	@Override
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    // This function take the name of the device to instantiate the barcodeReader
+		// This function take the name of the device to instantiate the barcodeReader
 		if ("selectDevice".equals(action)) {
 			Log.d(TAG, "select device");
 			// We expose the current register context
 			currentCallbackContext = callbackContext;
-      String deviceName = (args.length() == 0 || args.getString(0).equals("null")) ? "default" : args.getString(0);
+			String deviceName = (args.length() == 0 || args.getString(0).equals("null")) ? "default" : args.getString(0);
 
-      if (manager == null) {
-        callbackContext.error("No manager found please");
-      } else if (deviceName != null && !deviceName.equals(mConnectedScanner)) {
-        // We store the deviceName
-        mConnectedScanner = deviceName;
+			if (manager == null) {
+				callbackContext.error("No manager found please");
+			} else if (deviceName != null && !deviceName.equals(mConnectedScanner)) {
+				// We store the deviceName
+				mConnectedScanner = deviceName;
 
-        // We removing the old barcodeReader if exist
-        if (barcodeReader != null) {
-            barcodeReader.release();
-            barcodeReader.close();
-        }
+				// We removing the old barcodeReader if exist
+				if (barcodeReader != null) {
+					barcodeReader.release();
+					barcodeReader.close();
+				}
 
 				Log.d(TAG, "selected device" + deviceName);
 
-        barcodeReader = (deviceName.equals("default")) ? manager.createBarcodeReader() : manager.createBarcodeReader(deviceName);
+				barcodeReader = (deviceName.equals("default")) ? manager.createBarcodeReader() : manager.createBarcodeReader(deviceName);
 
-        if (barcodeReader != null) {
-          // We are binding the current context for barcode events
-          barcodeReader.addBarcodeListener(Aidc.this);
-          callbackContext.success();
-        } else {
-          callbackContext.error("Failed to open barcode reader with name " + deviceName);
-        }
+				if (barcodeReader != null) {
+					// We are binding the current context for barcode events
+					barcodeReader.addBarcodeListener(Aidc.this);
+					try {
+						// apply settings
+						barcodeReader.setProperty(BarcodeReader.PROPERTY_EAN_8_ENABLED, true);
+						barcodeReader.setProperty(BarcodeReader.PROPERTY_EAN_8_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+						barcodeReader.setProperty(BarcodeReader.PROPERTY_EAN_13_ENABLED, true);
+						barcodeReader.setProperty(BarcodeReader.PROPERTY_EAN_13_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+						barcodeReader.setProperty(BarcodeReader.PROPERTY_UPC_A_ENABLE, true);
+					} catch (UnsupportedPropertyException e) {
+						callbackContext.error(e.getMessage());
+					}
+					callbackContext.success();
+				} else {
+					callbackContext.error("Failed to open barcode reader with name " + deviceName);
+				}
 			}
 
 			return true;
 		} else if ("claim".equals(action)) {
-      // We claim the access to the device
+			// We claim the access to the device
 			if (barcodeReader != null) {
 				try {
 					barcodeReader.claim();
@@ -106,10 +116,10 @@ public class Aidc extends CordovaPlugin implements BarcodeListener {
 
 			return true;
 		} else if ("release".equals(action)) {
-      // We release the barcodeReader if exist
+			// We release the barcodeReader if exist
 			if (barcodeReader != null) {
 				barcodeReader.release();
-        mConnectedScanner = null;
+				mConnectedScanner = null;
 				callbackContext.success();
 			} else {
 				callbackContext.error("Reader not open");
@@ -117,8 +127,8 @@ public class Aidc extends CordovaPlugin implements BarcodeListener {
 
 			return true;
 		} else if ("register".equals(action)) {
-      //We register the current callback context
-      currentCallbackContext = callbackContext;
+			//We register the current callback context
+			currentCallbackContext = callbackContext;
 
 			PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
 			result.setKeepCallback(true);
@@ -126,51 +136,51 @@ public class Aidc extends CordovaPlugin implements BarcodeListener {
 
 			return true;
 		} else if ("unregister".equals(action)) {
-      // We unregister the current callback context
+			// We unregister the current callback context
 			currentCallbackContext = null;
 
 			return true;
 		} else if ("enableTrigger".equals(action)) {
-      // Enable the device trigger (true by default)
+			// Enable the device trigger (true by default)
 			Log.d(TAG, "enableTrigger");
 
-      if (barcodeReader == null) {
-        callbackContext.error("no barcode reader initalized");
-      } else {
-        try {
-				  barcodeReader.setProperty(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE, BarcodeReader.TRIGGER_CONTROL_MODE_AUTO_CONTROL);
-				  callbackContext.success();
-			  } catch (UnsupportedPropertyException e) {
-				  callbackContext.error(e.getMessage());
-			  }
-      }
+			if (barcodeReader == null) {
+				callbackContext.error("no barcode reader initalized");
+			} else {
+				try {
+					barcodeReader.setProperty(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE, BarcodeReader.TRIGGER_CONTROL_MODE_AUTO_CONTROL);
+					callbackContext.success();
+				} catch (UnsupportedPropertyException e) {
+					callbackContext.error(e.getMessage());
+				}
+			}
 
 			return true;
 		} else if ("disableTrigger".equals(action)) {
-      // Disable the device trigger
+			// Disable the device trigger
 			Log.d(TAG, "disableTrigger");
 
-      if (barcodeReader == null) {
-        callbackContext.error("no barcode reader initalized");
-      } else {
-        try {
-				  barcodeReader.setProperty(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE, BarcodeReader.TRIGGER_CONTROL_MODE_DISABLE);
-				  callbackContext.success();
-			  } catch (UnsupportedPropertyException e) {
-				  callbackContext.error(e.getMessage());
-			  }
+			if (barcodeReader == null) {
+				callbackContext.error("no barcode reader initalized");
+			} else {
+				try {
+					barcodeReader.setProperty(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE, BarcodeReader.TRIGGER_CONTROL_MODE_DISABLE);
+					callbackContext.success();
+				} catch (UnsupportedPropertyException e) {
+					callbackContext.error(e.getMessage());
+				}
 
-			  callbackContext.success();
-      }
+				callbackContext.success();
+			}
 
 			return true;
 		} else if ("listConnectedBarcodeDevices".equals(action)) {
-      // Return the list of connected barcode Devices
+			// Return the list of connected barcode Devices
 			Log.d(TAG, "listConnectedBarcodeDevices");
 
-      if (manager == null) {
-        callbackContext.error("Unable to find aidc manager");
-      } else {
+			if (manager == null) {
+				callbackContext.error("Unable to find aidc manager");
+			} else {
 				this.cordova.getThreadPool().execute(new Runnable() {
 					public void run() {
 						List<BarcodeReaderInfo> list = manager.listConnectedBarcodeDevices();
@@ -197,7 +207,7 @@ public class Aidc extends CordovaPlugin implements BarcodeListener {
 						}
 					}
 				});
-      }
+			}
 
 			return true;
 		}
